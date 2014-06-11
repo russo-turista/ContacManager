6af6aa9309592146891717ca63b041ce28617371
russo-turista <fjobmail@gmail.com>
2014-06-11 17:40:40 +0400

    Added startActivity

---
+++ src/com/contactmanger/WelcomeActivity.java
@@ -0,0 +1,20 @@
+package com.contactmanger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class WelcomeActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.startpage);
+		
+		LayoutInflater ltInflater = getLayoutInflater();
+        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
+        View footerView = ltInflater.inflate(R.layout.footer, relLayout, true);
+        //relLayout.addView(footerView);
+	}
+
+}
