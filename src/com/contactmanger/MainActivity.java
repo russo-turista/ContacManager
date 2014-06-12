caf4bd41387d22bf3e2283cdcf958b617fad3bb4
Gena <fjobmail@gmail.com>
2014-06-12 16:49:17 +0400

    Added DataInput Activity

--- src/com/contactmanger/MainActivity.java
+++ src/com/contactmanger/MainActivity.java
@@ -1,15 +1,75 @@
 package com.contactmanger;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener{
+	Boolean isFavorite;
+	Button btnWelcomPage;
+	String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+		      "Костя", "Игорь", "Анна", "Денис", "Андрей" };
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		LayoutInflater ltInflater = getLayoutInflater();
+		RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.contactsLayout);
+		View headerView = ltInflater.inflate(R.layout.header, relLayout, true);
+		
+		 ListView lvMain = (ListView) findViewById(R.id.listContacts);
+
+		    // создаем адаптер
+		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		        android.R.layout.simple_list_item_1, names);
+
+		    // присваиваем адаптер списку
+		    lvMain.setAdapter(adapter);
+		    
+		View footerView = ltInflater.inflate(R.layout.footer, relLayout, true);
+		
+		 btnWelcomPage = (Button) findViewById(R.id.btnWelcome);
+		 btnWelcomPage.setOnClickListener(this);
+		
+		Intent intent = getIntent();
+		isFavorite = intent.getBooleanExtra("favorite", false);
+		if (isFavorite) {
+			Toast.makeText(this, isFavorite.toString(), Toast.LENGTH_LONG)
+					.show();
+		} else {
+			Toast.makeText(this, isFavorite.toString(), Toast.LENGTH_LONG)
+					.show();
+		}
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		 switch (v.getId()) {
+		    case R.id.btnWelcome:
+		    	Intent intent_welcomPage = new Intent(this, WelcomeActivity.class);
+		    	startActivity(intent_welcomPage);
+		    	//Toast.makeText(this, "Нажата кнопка allcontactsB", Toast.LENGTH_LONG).show();		    	
+		      break;
+		    case R.id.btnFavoriteList:
+		    	Intent intent_favorite = new Intent(this, MainActivity.class);
+		    	intent_favorite.putExtra("favorite", true);
+		    	startActivity(intent_favorite);
+		    	//Toast.makeText(this, "Нажата кнопка favoriteB", Toast.LENGTH_LONG).show();
+		    	break;
+		    default:
+		      break;
+		    }
+		
 	}
 
 }
--- src/com/contactmanger/WelcomeActivity.java
+++ src/com/contactmanger/WelcomeActivity.java
@@ -1,20 +1,56 @@
 package com.contactmanger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends Activity implements OnClickListener {
+	private static final String TAG = "myLogs";
+	Button btnAllcontactList;
+	Button btnFavoriteList;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startpage);
 		
 		LayoutInflater ltInflater = getLayoutInflater();
-        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.mainRelLayout);
+        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.welcomPageRelLayout);
         View footerView = ltInflater.inflate(R.layout.footer, relLayout, true);
-        //relLayout.addView(footerView);
+        btnAllcontactList = (Button) findViewById(R.id.btnAllcontactsLis);
+        btnFavoriteList = (Button) findViewById(R.id.btnFavoriteList);
+        
+        btnAllcontactList.setOnClickListener(this);
+        btnFavoriteList.setOnClickListener(this);
+        Log.d(TAG, "WelcomeActivity load");
+	}
+
+	@Override
+	public void onClick(View v) {
+			Log.d(TAG, "onClick work");
+		 switch (v.getId()) {
+		    case R.id.btnAllcontactsLis:
+		    	Intent intent_allcontacts = new Intent(this, MainActivity.class);
+		    	intent_allcontacts.putExtra("favorite", false);
+		    	startActivity(intent_allcontacts);
+		    	//Toast.makeText(this, "Нажата кнопка allcontactsB", Toast.LENGTH_LONG).show();
+		    	
+		      break;
+		    case R.id.btnFavoriteList:
+		    	Intent intent_favorite = new Intent(this, MainActivity.class);
+		    	intent_favorite.putExtra("favorite", true);
+		    	startActivity(intent_favorite);
+		    	//Toast.makeText(this, "Нажата кнопка favoriteB", Toast.LENGTH_LONG).show();
+		    	break;
+		    default:
+		      break;
+		    }
+		
 	}
 
 }
