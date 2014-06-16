7717384204cd133c3721b180591e1682317ad9f3
Gena <fjobmail@gmail.com>
2014-06-16 22:41:49 +0400

    Add Data base adapter

--- gen/com/contactmanger/R.java
+++ gen/com/contactmanger/R.java
@@ -37,23 +37,25 @@ public final class R {
         public static final int ic_launcher=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
+        public static final int action_settings=0x7f09000d;
         public static final int btnAllcontactsLis=0x7f090001;
         public static final int btnFavoriteList=0x7f090003;
         public static final int btnUpload=0x7f090006;
         public static final int btnWelcome=0x7f090002;
-        public static final int content=0x7f09000a;
+        public static final int content=0x7f09000c;
         public static final int footer=0x7f090000;
         public static final int fullContacts=0x7f090005;
         public static final int header=0x7f090004;
-        public static final int listContacts=0x7f090007;
-        public static final int listContent=0x7f090008;
-        public static final int welcomPageRelLayout=0x7f090009;
+        public static final int ivImg=0x7f090007;
+        public static final int listContacts=0x7f09000a;
+        public static final int listContent=0x7f090009;
+        public static final int tvText=0x7f090008;
+        public static final int welcomPageRelLayout=0x7f09000b;
     }
     public static final class layout {
         public static final int footer=0x7f030000;
         public static final int header=0x7f030001;
-        public static final int list=0x7f030002;
+        public static final int item=0x7f030002;
         public static final int main=0x7f030003;
         public static final int startpage=0x7f030004;
     }
