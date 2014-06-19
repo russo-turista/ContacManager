d4152e530c6c51c547aa186a20563a832944a9bd
russo-turista <fjobmail@gmail.com>
2014-06-19 17:25:17 +0400

    Added photo with camera

--- src/com/contactmanger/DataInput.java
+++ src/com/contactmanger/DataInput.java
@@ -1,12 +1,19 @@
 package com.contactmanger;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +25,10 @@ public class DataInput extends Activity implements android.view.View.OnClickList
 	final String LOG_TAG = "myLogs";
 	final String HEADER_DIALOG = "Select image source";
 	final int DIALOG_ITEMS = 0;
+	
+	final int CAMERA_REQUEST = 0;
+	final int GALLERY_REQUEST = 1;
+	
 	String dataDialog[] = { "From Camera", "From SD Card" };
 	
 	Button btnCancel;
@@ -59,6 +70,19 @@ public class DataInput extends Activity implements android.view.View.OnClickList
 
 OnClickListener dialogClickListener = new OnClickListener() {
 		public void onClick(DialogInterface dialog, int which) {
+			switch (which) {
+			case GALLERY_REQUEST:
+				Intent photoGalleryIntent = new Intent(Intent.ACTION_PICK);
+				photoGalleryIntent.setType("image/*");
+				startActivityForResult(photoGalleryIntent, GALLERY_REQUEST);    
+				break;
+			case CAMERA_REQUEST:
+				Intent photoCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(photoCameraIntent, CAMERA_REQUEST);    
+				break;	
+			default:
+				break;
+			}
 			
 			Log.d(LOG_TAG, "which = " + which);
 		}
@@ -75,5 +99,34 @@ OnClickListener dialogClickListener = new OnClickListener() {
 			break;
 		}
 		
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) { 
+	    super.onActivityResult(requestCode, resultCode, imageReturnedIntent); 
+	    
+	    Bitmap itemPic = null;
+
+	    switch(requestCode) { 
+	    case CAMERA_REQUEST:
+	    	itemPic = (Bitmap) imageReturnedIntent.getExtras().get("data");
+	    	imgUpload.setImageBitmap(itemPic);
+	    	break;
+	    case GALLERY_REQUEST:
+	        if(resultCode == RESULT_OK){  
+	            Uri selectedImage = imageReturnedIntent.getData();
+	            try {
+	            	itemPic = Media.getBitmap(getContentResolver(), selectedImage);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	            imgUpload.setImageBitmap(itemPic);
+	        }
+	        break;
+	    default:
+	    	break;
+	    }
+	    	
 	}
 }
