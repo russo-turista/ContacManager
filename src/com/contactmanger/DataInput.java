75e781cdb68f63f310088397ac67f03d09f52aca
russo-turista <fjobmail@gmail.com>
2014-06-20 17:52:57 +0400

    Fixed dialog window

--- src/com/contactmanger/DataInput.java
+++ src/com/contactmanger/DataInput.java
@@ -1,18 +1,14 @@
 package com.contactmanger;
 
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.provider.MediaStore.Images.Media;
 import android.util.Log;
 import android.view.View;
@@ -23,20 +19,16 @@ import android.widget.TextView;
 
 public class DataInput extends Activity implements android.view.View.OnClickListener{
 	final String LOG_TAG = "myLogs";
-	final String HEADER_DIALOG = "Select image source";
-	final int DIALOG_ITEMS = 0;
-	
-	final int CAMERA_REQUEST = 0;
-	final int GALLERY_REQUEST = 1;
-	
-	String dataDialog[] = { "From Camera", "From SD Card" };
 	
 	Button btnCancel;
 	Button btnSend;
 	Button deleteText;
+	
 	TextView lengthDescription;
 	EditText editText;
 	ImageView imgUpload;
+	Bitmap itemPic = null;
+	Uri imagePath;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,105 +36,50 @@ public class DataInput extends Activity implements android.view.View.OnClickList
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.upload_photo);
 
-		showDialog(DIALOG_ITEMS);
 		
 		btnCancel = (Button) findViewById(R.id.btnCancel);
 		btnSend = (Button) findViewById(R.id.btnSend);
-		imgUpload = (ImageView) findViewById(R.id.imgUpload);
 		
+		imgUpload = (ImageView) findViewById(R.id.imgUpload);
 		deleteText = (Button) findViewById(R.id.deleteText);
 		editText = (EditText) findViewById(R.id.editText);
 		lengthDescription = (TextView) findViewById(R.id.lengthDescription);
 		
+		
 		btnCancel.setOnClickListener(this);
 		btnSend.setOnClickListener(this);
 		deleteText.setOnClickListener(this);
+		
+		Intent intent = getIntent();
+		Log.d(LOG_TAG, "path image in DataInput Activity = " + intent.getStringExtra("imagePath"));
+		imagePath = intent.getParcelableExtra("imagePath");
+		//imgUpload.setImageURI(imagePath);
+		
+        try {
+        	itemPic = Media.getBitmap(getContentResolver(), imagePath);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        imgUpload.setImageBitmap(itemPic);
 	}
 
-	protected Dialog onCreateDialog(int id) {
-		AlertDialog.Builder adb = new AlertDialog.Builder(this);
-		if (id == DIALOG_ITEMS) {
-			adb.setTitle(HEADER_DIALOG);
-			adb.setItems(dataDialog, dialogClickListener);
-		}
-		return adb.create();
-	}
-
-OnClickListener dialogClickListener = new OnClickListener() {
-		public void onClick(DialogInterface dialog, int which) {
-			switch (which) {
-			case GALLERY_REQUEST:
-				Intent photoGalleryIntent = new Intent(Intent.ACTION_PICK);
-				photoGalleryIntent.setType("image/*");
-				startActivityForResult(photoGalleryIntent, GALLERY_REQUEST);    
-				break;
-			case CAMERA_REQUEST:
-				Intent photoCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				startActivityForResult(photoCameraIntent, CAMERA_REQUEST);    
-				break;	
-			default:
-				break;
-			}
-			
-			Log.d(LOG_TAG, "which = " + which);
-		}
-	};
 
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btnCancel:
-			Intent intent_allcontacts = new Intent(this, MainActivity.class);
+			/*Intent intent_allcontacts = new Intent(this, MainActivity.class);
 	    	intent_allcontacts.putExtra("favorite", false);
-	    	startActivity(intent_allcontacts);
+	    	startActivity(intent_allcontacts);*/
+			super.onBackPressed();
+			break;
+		case R.id.btnSend:
 			break;
 		default:
 			break;
 		}
 		
 	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
-	    super.onActivityResult(requestCode, resultCode, data); 
-	    
-	    Bitmap itemPic = null;
-	    if (requestCode == CAMERA_REQUEST || requestCode == GALLERY_REQUEST){
-	    	if (resultCode == RESULT_OK){
-	    		Uri selectedImage = data.getData();
-	            try {
-	            	itemPic = Media.getBitmap(getContentResolver(), selectedImage);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	            imgUpload.setImageURI(selectedImage);;
-	            Log.d(LOG_TAG, "path image = " + selectedImage.getPath());
-	    	}	
-	    }
-	    /*switch(requestCode) { 
-	    case CAMERA_REQUEST:
-	    	if (resultCode == RESULT_OK){
-	    		itemPic = (Bitmap) data.getExtras().get("data");
-	    		Uri selectedImage = data.getData();
-	    		imgUpload.setImageBitmap(itemPic);
-	    	}	
-	    	break;
-	    case GALLERY_REQUEST:
-	        if(resultCode == RESULT_OK){  
-	            Uri selectedImage = data.getData();
-	            try {
-	            	itemPic = Media.getBitmap(getContentResolver(), selectedImage);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	            imgUpload.setImageBitmap(itemPic);
-	        }
-	        break;
-	    default:
-	    	break;
-	    }*/
-	    	
-	}
+	
 }
--- src/com/contactmanger/MainActivity.java
+++ src/com/contactmanger/MainActivity.java
@@ -1,17 +1,38 @@
 package com.contactmanger;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
+	
+	final String LOG_TAG = "myLogs";
+	
+	final String HEADER_DIALOG = "Select image source";
+	final int DIALOG_ITEMS = 0;
+	
+	final int CAMERA_REQUEST = 0;
+	final int GALLERY_REQUEST = 1;
+	
+	String dataDialog[] = { "From Camera", "From SD Card" };
 	
 	Button btnWelcomPage;
 	Button btnUploadPhoto;
@@ -56,6 +77,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		btnUploadPhoto.setOnClickListener(this);
 		
 		Intent intent = getIntent();
+		
 		isFavorite = intent.getBooleanExtra("favorite", false);
 		if (isFavorite) {
 			Toast.makeText(this, isFavorite.toString(), Toast.LENGTH_LONG)
@@ -66,6 +88,37 @@ public class MainActivity extends Activity implements OnClickListener {
 		}
 
 	}
+	
+	protected Dialog onCreateDialog(int id) {
+		AlertDialog.Builder adb = new AlertDialog.Builder(this);
+		if (id == DIALOG_ITEMS) {
+			adb.setTitle(HEADER_DIALOG);
+			adb.setItems(dataDialog, dialogClickListener);
+		}
+		return adb.create();
+	}
+
+	OnClickListener dialogClickListener = new OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				switch (which) {
+				case GALLERY_REQUEST:
+					Log.d(LOG_TAG, "GALLERY_REQUEST = " + which);
+					Intent photoGalleryIntent = new Intent(Intent.ACTION_PICK);
+					photoGalleryIntent.setType("image/*");
+					startActivityForResult(photoGalleryIntent, GALLERY_REQUEST);    
+					break;
+				case CAMERA_REQUEST:
+					Log.d(LOG_TAG, "CAMERA_REQUEST = " + which);
+					Intent photoCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+					startActivityForResult(photoCameraIntent, CAMERA_REQUEST);    
+					break;	
+				default:
+					break;
+				}
+				
+				Log.d(LOG_TAG, "which = " + which);
+			}
+		};
 
 	@Override
 	public void onClick(View v) {
@@ -82,14 +135,36 @@ public class MainActivity extends Activity implements OnClickListener {
 			// Toast.makeText(this, "favoriteB", Toast.LENGTH_LONG).show();
 			break;
 		case R.id.btnUpload:
-			Intent intent_dataInput = new Intent(this, DataInput.class);
-			startActivity(intent_dataInput);
+			showDialog(DIALOG_ITEMS);
 			break;
 		default:
 			break;
 		}
 
 	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
+	    super.onActivityResult(requestCode, resultCode, data); 
+	    
+	    Bitmap itemPic = null;
+	    if (requestCode == CAMERA_REQUEST || requestCode == GALLERY_REQUEST){
+	    	if (resultCode == RESULT_OK){
+	    		Uri selectedImage = data.getData();
+	            try {
+	            	itemPic = Media.getBitmap(getContentResolver(), selectedImage);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	            //imgUpload.setImageURI(selectedImage);
+	            Log.d(LOG_TAG, "path image = " + selectedImage.getPath());
+	            Intent intent_dataInput = new Intent(this, DataInput.class);
+	            intent_dataInput.putExtra("imagePath", selectedImage);
+				startActivity(intent_dataInput);
+	    	}	
+	    }
+	}    
 	 protected void onDestroy() {
 		    super.onDestroy();
 		    // закрываем подключение при выходе
